99887fa585e4fd29953c158df1a03cf51117e4c7
Gediminas Paulauskas <menesis@delfi.lt>
2001-11-27 01:11:43 +0000

    define GETTEXT_PACKAGE use GETTEXT_PACKAGE and UTF-8.
    2001-11-27  Gediminas Paulauskas <menesis@delfi.lt>
    
    	* acconfig.h, configure.in: define GETTEXT_PACKAGE
    	* all games: use GETTEXT_PACKAGE and UTF-8.

--- gnotski.c
+++ gnotski.c
@@ -481,8 +481,9 @@ int main (int argc, char **argv){
   GnomeClient *client;
 
   gnome_score_init(APPNAME);
-  bindtextdomain(PACKAGE, GNOMELOCALEDIR);
-  textdomain(PACKAGE);
+  bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  textdomain(GETTEXT_PACKAGE);
    
   gnome_init_with_popt_table(APPNAME, VERSION, argc, argv, options, 0, NULL);
   gnome_window_icon_set_default_from_file (GNOME_ICONDIR"/gnotski-icon.png");
