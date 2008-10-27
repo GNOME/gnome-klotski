c1342e1d7c7898fe5fa55c912d59ba06167bf7f1
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-27 00:16:13 +0000

    move common help functionality to libgames-support/games-help.[ch]
    svn path=/trunk/; revision=8205

--- gnotski.c
+++ gnotski.c
@@ -30,6 +30,7 @@
 
 #include <libgames-support/games-conf.h>
 #include <libgames-support/games-gridframe.h>
+#include <libgames-support/games-help.h>
 #include <libgames-support/games-preimage.h>
 #include <libgames-support/games-runtime.h>
 #include <libgames-support/games-scores.h>
@@ -1537,26 +1538,7 @@ prev_level_cb (GtkAction * action)
 void
 help_cb (GtkAction * action)
 {
-  GdkScreen *screen;
-  GError *error = NULL;
-
-  screen = gtk_widget_get_screen (GTK_WIDGET (window));
-  gtk_show_uri (screen, "ghelp:gnotski", gtk_get_current_event_time (), &error);
-
-  if (error != NULL)
-  {
-    GtkWidget *d;
-    d = gtk_message_dialog_new (GTK_WINDOW (window), 
-                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                              GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, 
-                              "%s", _("Unable to open help file"));
-    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (d),
-                              "             %s", error->message);
-    g_signal_connect (d, "response", G_CALLBACK (gtk_widget_destroy), NULL);
-    gtk_window_present (GTK_WINDOW (d));
-
-    g_error_free (error);
-  }
+  games_help_display (window, "gnotski", NULL);
 }
 
 void
